f37ab161b53ee75a97a80d6152df38a9a4667c1d
Nikolay Shishkin <nikolson99999@gmail.com>
2022-10-31 22:55:08 +0300

    Доделал + адаптировал первую страницу

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+// Открытие и закрытие меню бургера
+const menuOpenButton = document.querySelector(".menu__open-button");
+const menuPopup = document.querySelector(".menu");
+const menuCloseButton = document.querySelector(".menu__close-button");
+
+function openPopup (menuElement) {
+  menuElement.classList.add("menu__open")
+}
+
+function closePopup (popupElement) {
+  popupElement.classList.remove("menu__open")
+}
+
+menuOpenButton.addEventListener("click", function(evt) {
+  openPopup(menuPopup);
+});
+
+menuCloseButton.addEventListener("click", function(evt) {
+  closePopup(menuPopup);
+});
+
