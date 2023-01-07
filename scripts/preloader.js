e8bb1a6a0e3044d41b6c7a227ddd7eb8fe75b4a9
Nikolay Shishkin <nikolson99999@gmail.com>
2023-01-07 22:30:02 +0300

    new loader

--- scripts/preloader.js
+++ scripts/preloader.js
@@ -1,21 +1,21 @@
 // Функция будет считать проценты от 1 до 100
 
-function progress(){
-    const percent = document.querySelector('.percent');
-    let count = 4;
-    let loading = setInterval(animate, 17);
+// function progress(){
+//     const percent = document.querySelector('.percent');
+//     let count = 4;
+//     let loading = setInterval(animate, 17);
 
-    function animate(){
-      if(count == 100){
-        clearInterval(loading);
-      }else{
-        count = count + 1;
-        percent.textContent = count + '%';
-      }
-    }
-}
+//     function animate(){
+//       if(count == 100){
+//         clearInterval(loading);
+//       }else{
+//         count = count + 1;
+//         percent.textContent = count + '%';
+//       }
+//     }
+// }
 
-progress();
+// progress();
 
 
 // Функция будет скрывать загрузку, через время какое поставлю
